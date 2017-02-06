9a96bbb95e31795a9d2090589a06605f76ccb491
Vimanyu Aggarwal <vimanyu1022007@gmail.com>
2017-02-05 23:53:15 -0800

    Delete routes.js

--- js/routes.js
+++
@@ -1,9 +0,0 @@
-// angular.module('networkSnifferApp',['ngRoute']).config(function($routeProvider){
-//     $routeProvider
-//         .when('/plotter',{
-//             templateURL: "templates/data.html",
-//             controller: 'PlotterCtrl'
-//         })
-//         .otherwise({redirectTo: '/plotter'});
-//
-// });
